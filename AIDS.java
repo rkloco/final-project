2832916bb76a5377514aaebb373fa8298620393f
Ronit Kapur <81713063+rkloco@users.noreply.github.com>
2025-04-21 10:05:36 -0400

    Add files via upload

---
+++ AIDS.java
@@ -0,0 +1,30 @@
+public interface AIDS {
+
+    /**
+    * Returns all records that exactly match the specified
+    criteria.
+    * @param attribute The attribute/field to query on
+    * @param value The exact value to match
+    * @return A collection of records matching the criteria
+    */
+    List<T> findDeaths(String attribute, Object value);
+
+    /**
+    * Returns all records where the specified attribute falls
+    within the given range.
+    * @param attribute The attribute/field to query on
+    * @param lowerBound The lower bound of the range (inclusive)
+    * @param upperBound The upper bound of the range (inclusive)
+    * @return A collection of records matching the criteria
+    */
+    List<T> findDeathsInYearRange(String country, int lowerBound, int upperBound);
+
+    /**
+    * Returns the average value of the specified attribute during
+    a given time frame
+    * @param attribute The attribute/field
+    * @param startTime The start time of the period (inclusive)
+    * @param endTime The end time of the period (inclusive)
+    * @return The statistic calculated */
+    List<T> averageDeathsPerYear(String country, int startYear, int endYear);
+}
